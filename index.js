a0c74b266c5931582ca08e2498a9f205091a746e
ismail-dev-code <hm.ismail772@gmail.com>
2025-05-31 23:06:58 +0600

    chore: token firebase + jwt

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const http = require("http");
-const { Server } = require("socket.io");
+var jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 const port = process.env.PORT || 3000;
 require("dotenv").config();
@@ -9,17 +10,50 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+var admin = require("firebase-admin");
 
-// create http server for socket.io to bind on
-const server = http.createServer(app);
+var serviceAccount = require("./firebase-admin-key.json");
 
-// initialize socket.io server with CORS config
-const io = new Server(server, {
-  cors: {
-    origin: "*", // allow all origins for development, change in production
-    methods: ["GET", "POST"],
-  },
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
 });
+
+const logger = (req, res, next) => {
+  console.log("inside the logger middleware");
+  next();
+};
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+const verifyFirebaseToken = async (req, res, next) => {
+  const authHeader = req.headers?.authorization;
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const userInfo = await admin.auth().verifyIdToken(token);
+  req.tokenEmail = userInfo.email;
+  next();
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pw0rah1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -42,6 +76,22 @@ async function run() {
       .db("career_application")
       .collection("applications");
 
+    // jwt token related APIs
+    app.post("/jwt", async (req, res) => {
+      const userInfo = req.body;
+
+      const token = jwt.sign(userInfo, process.env.JWT_ACCESS_SECRET, {
+        expiresIn: "2h",
+      });
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+      });
+
+      res.send({ success: true });
+    });
+
     // jobs api
     app.get("/jobs", async (req, res) => {
       const email = req.query.email;
@@ -60,6 +110,21 @@ async function run() {
     //   const result = await jobsCollection.find(query).toArray();
     //   res.send(result);
     // });
+    app.get("/jobs/applications", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const query = { hr_email: email };
+      const jobs = await jobsCollection.find(query).toArray();
+
+      // should use aggregate to have optimum data fetching
+      for (const job of jobs) {
+        const applicationQuery = { jobId: job._id.toString() };
+        const application_count = await applicationsCollection.countDocuments(
+          applicationQuery
+        );
+        job.application_count = application_count;
+      }
+      res.send(jobs);
+    });
 
     // kono ekta nirdishto data pabar jonno
     app.get("/jobs/:id", async (req, res) => {
@@ -74,7 +139,6 @@ async function run() {
       const result = await jobsCollection.insertOne(newJob);
       res.send(result);
     });
-    
 
     // job applications related APIs
     // app.get("/applications", async (req, res) => {
@@ -84,7 +148,7 @@ async function run() {
     //   };
     //   const result = await applicationsCollection.find(query).toArray();
 
-      // bad way to aggregate data
+    // bad way to aggregate data
     //   for (const application of result) {
     //     const jobId = application.jobId;
     //     const jobQuery = { _id: new ObjectId(jobId) };
@@ -102,8 +166,12 @@ async function run() {
     // });
 
     // another way start here
-    app.get("/applications", async (req, res) => {
+    // job applications related APIs
+    app.get("/applications", logger, verifyFirebaseToken, async (req, res) => {
       const email = req.query.email;
+      if (req.tokenEmail != email) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
       const query = { applicantEmail: email };
 
       const applications = await applicationsCollection.find(query).toArray();
@@ -132,22 +200,21 @@ async function run() {
     });
     // another way end here
 
-     app.post('/applications', async (req, res) => {
+    app.post("/applications", async (req, res) => {
       const application = req.body;
       // console.log(application);
       const result = await applicationsCollection.insertOne(application);
       res.send(result);
     });
 
-
     // app.get('/applications/:id', () =>{})
-    app.get('/applications/job/:job_id', async (req, res) => {
+    app.get("/applications/job/:job_id", async (req, res) => {
       const job_id = req.params.job_id;
       // console.log(job_id);
-      const query = { jobId: job_id }
+      const query = { jobId: job_id };
       const result = await applicationsCollection.find(query).toArray();
       res.send(result);
-    })
+    });
 
     app.patch("/applications/:id", async (req, res) => {
       const id = req.params.id;
@@ -168,24 +235,6 @@ async function run() {
       const result = await applicationsCollection.deleteOne(query);
       res.send(result);
     });
-
-    // Socket.IO connection and chat message handling start
-    io.on("connection", (socket) => {
-      console.log("New client connected:", socket.id);
-
-      // Listen for chat messages from clients
-      socket.on("chat message", (msg) => {
-        console.log("Message received:", msg);
-
-        // Broadcast the message to all other clients except sender
-        socket.broadcast.emit("chat message", msg);
-      });
-
-      socket.on("disconnect", () => {
-        console.log("Client disconnected:", socket.id);
-      });
-    });
-    // Socket.IO connection and chat message handling end here
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
