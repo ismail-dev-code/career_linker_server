b51cc78783de05d32d47f90ae437776af1d7ff74
ismail-dev-code <hm.ismail772@gmail.com>
2025-06-04 16:18:37 +0600

    feat: live site added

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 // middleware
-app.use(cors());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://career-linker-client.vercel.app", "http://localhost:5173", "https://careerlinker-bd.web.app"],
     credentials: true,
   })
 );
 app.use(cookieParser());
-var admin = require("firebase-admin");
+const admin = require("firebase-admin");
 
-var serviceAccount = require("./firebase-admin-key.json");
+const serviceAccount = require("./firebase-admin-key.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -69,7 +69,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const jobsCollection = client.db("career_linker").collection("jobs");
     const applicationsCollection = client
@@ -237,10 +237,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
